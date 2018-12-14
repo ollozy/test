a82ad33a37326e748042599851b7c165f5ff3dfe
user1 <user1@A518-12>
2018-12-14 11:52:59 +0300

    yes

--- test/Program.cs
+++ test/Program.cs
@@ -10,7 +10,7 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Знакомство с гитхаб");
+            Console.WriteLine("Знакомство с GitHub");
         }
     }
 }
